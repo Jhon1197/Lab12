4be2d103d1ddf97be63f3f51c850275276d0792d
Jhon1197 <jhon.alvarez.ttc.771@unilibre.edu.co>
2024-11-14 20:08:19 -0500

    Cambios

--- punto1.js
+++ punto1.js
@@ -1,11 +1,16 @@
 let numeros = [5,1,8,6,7,8,6,9];
 let sumatoria=0;
 let promedio=0;
-for (i=0; i<numeros.length;i++){
+for (let i=0; i<numeros.length;i++){
     console.log(numeros[1]);
     sumatoria+=numeros[i];
 }
+console.log(sumatoria);
 promedio=sumatoria/numeros.length;
 
-document.getElementById("resultado").value= sumatoria;
-document.getElementById("promedio").value= promedio;
+document.write("Este es el resultado de la sumatoria " , sumatoria,"<br>");
+document.write("Este es el resultado de la promedio " , promedio,"<br>");
+
+document.getElementById("resultado").value("jjjj");
+ 
+//document.getElementById("promedio").value = promedio;
